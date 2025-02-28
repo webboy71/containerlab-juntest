d18c953ff0cb59478574bb85168009e6e828410d
Dan Caldwell <webboy@gmail.com>
2025-02-28 11:26:11 +0100

    Update startup.sh

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -eux
-mkdir CLAB && cd CLAB && git clone https://github.com/hellt/vrnetlab.git && cd vrnetlab/vjunosrouter && wget "https://cdn.juniper.net/software/vJunos-Router/24.2R1-S2/vJunos-router-24.2R1-S2.5.qcow2?SM_USER=anon&__gda__=1740737072_54fb7f908b6473d199da7590f8efb712" && make
+mkdir CLAB && cd CLAB && git clone https://github.com/hellt/vrnetlab.git && cd vrnetlab/vjunosrouter && wget "https://cdn.juniper.net/software/vJunos-Router/24.2R1-S2/vJunos-router-24.2R1-S2.5.qcow2?SM_USER=anon&__gda__=1740737072_54fb7f908b6473d199da7590f8efb712" && mv vJunos* vJunos-router-24.2R1-S2.5.qcow2 && make
